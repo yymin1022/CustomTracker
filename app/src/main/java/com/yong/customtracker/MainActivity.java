452a71585f959afc64e9cd27ee1a01debe9f72b7
yymin1022 <yymin1022@gmail.com>
2020-03-27 22:51:37 +0900

    Add WelcomeActivity

--- app/src/main/java/com/yong/customtracker/MainActivity.java
+++ app/src/main/java/com/yong/customtracker/MainActivity.java
@@ -2,13 +2,21 @@ package com.yong.customtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor preferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("preference", MODE_PRIVATE);
+        if(sharedPreferences.getBoolean("isFirst", true)){
+            startActivity(getApplicationContext(), WelcomeActivity.class));
+        }
     }
 }
