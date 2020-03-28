aa387e55203ac53e481b075f4c064c40d4f65a2f
yymin1022 <yymin1022@gmail.com>
2020-03-28 16:58:04 +0900

    Update WelcomeActivity Prototype, MainActivity  Layout

--- app/src/main/java/com/yong/customtracker/WelcomeActivity.java
+++ app/src/main/java/com/yong/customtracker/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.yong.customtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnDone = findViewById(R.id.btn_welcome_done);
+        SharedPreferences sharedPreferences = getSharedPreferences("preference", MODE_PRIVATE);
+        final SharedPreferences.Editor preferenceEditor = sharedPreferences.edit();
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btn_welcome_done:
+                        preferenceEditor.putBoolean("isFirst", false);
+                        preferenceEditor.apply();
+                        finish();
+                }
+            }
+        };
     }
 }
